6130bbf35b475a9247ae6f0e8174709429df7876
Nuclearius <nuclearius.brasil@gmail.com>
2022-09-27 16:59:05 -0300

    login funfando
    login funfando, avisando o usuario quando campos estão vazios ou incorretos e guardando token e ID do usuário.

--- Front/lib/http.dart
+++ Front/lib/http.dart
@@ -3,9 +3,11 @@ import 'dart:ffi';
 import 'dart:io';
 
 import 'package:http/http.dart' as http;
+import 'package:jwt_decoder/jwt_decoder.dart';
 
 String token =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMjNhMWM0YWZhOTIwNTYyYzFmNTY0MSIsIm5vbWUiOiJ0ZXN0ZU5vbWUiLCJpYXQiOjE2NjM2MTM5MjJ9.3obJ_NUlTmHi2RkDNbMJ0TTJp_CK3iHBZ-ot9FNgaRY";
+String userID = "";
 
 Future<int> testServer() async {
   final response = await http.get(Uri.parse("http://10.0.2.2:3000/usuarios"),
@@ -16,6 +18,22 @@ Future<int> testServer() async {
     print(jsonDecode(response.body)[2]['nome'].toString());
   } else {
     throw Exception("falha em carregar usuarios");
+  }
+  return response.statusCode;
+}
+
+Future<int> login(String email, String senha) async {
+  final response = await http.post(Uri.parse("http://10.0.2.2:3000/login"),
+      headers: {"Content-type": "application/json"},
+      body: jsonEncode(<String, dynamic>{
+        'email': email,
+        'senha': senha,
+      }));
+  if (response.statusCode == 204) {
+    token = response.headers['authorization'].toString();
+    userID = JwtDecoder.decode(token)['id'];
+    print("token: " + token);
+    print("userID: " + userID);
   }
   return response.statusCode;
 }

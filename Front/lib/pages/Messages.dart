31969b85b929a8c883effeae8271fed876ef751c
Nuclearius <nuclearius.brasil@gmail.com>
2022-10-02 11:07:38 -0300

    novas telas
    tela de usuário adicionados feita, base para a tela de chat direto feita

--- Front/lib/pages/Messages.dart
+++ Front/lib/pages/Messages.dart
@@ -5,10 +5,69 @@ import 'package:fys/builders.dart';
 import 'package:fys/main.dart';
 import 'package:fys/pages/Comunities.dart';
 import 'package:fys/pages/ShootNPick.dart';
+import 'package:fys/pages/DirectChat.dart';
 
 double buttonWidth = 135;
 double buttonHeigth = 50;
 double fontsize = 17;
+
+class Member {
+  final int id;
+  final String name;
+  final String picture;
+
+  const Member({required this.id, required this.name, required this.picture});
+}
+
+List<Widget> MemberWidgetList(BuildContext context, List<Member> MemberList) {
+  var widgetList = <Widget>[];
+  int j = 0;
+  var rowList = <Widget>[];
+  for (int I = 0; I < MemberList.length; I++) {
+    rowList.add(
+      Container(
+        height: 160,
+        width: 100,
+        margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+        decoration:
+            BoxDecoration(border: Border.all(color: Colors.white, width: 1)),
+        child: ElevatedButton(
+          onPressed: () => PushScreen(context, chatPage(MemberList[I].id)),
+          style: ElevatedButton.styleFrom(primary: Color(0x00000000)),
+          child: Stack(
+            children: [
+              Image.asset(
+                MemberList[I].picture,
+                fit: BoxFit.scaleDown,
+              ),
+              Container(
+                alignment: Alignment.bottomCenter,
+                child: Text(
+                  MemberList[I].name,
+                  style: TextStyle(
+                    fontFamily: 'alagard',
+                    color: Colors.white,
+                    fontSize: 15,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+    j++;
+    if (j >= 3) {
+      widgetList.add(Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: rowList,
+      ));
+      j = 0;
+      rowList = <Widget>[];
+    }
+  }
+  return widgetList;
+}
 
 class MessagesPage extends StatefulWidget {
   const MessagesPage({Key? key}) : super(key: key);
@@ -18,6 +77,13 @@ class MessagesPage extends StatefulWidget {
 }
 
 class _MessagesPageState extends State<MessagesPage> {
+  Widget _mainpart = CircularProgressIndicator();
+
+  @override
+  void initState() {
+    LoadMembers();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,10 +99,14 @@ class _MessagesPageState extends State<MessagesPage> {
               icon: Icon(Icons.account_circle_outlined)),
         ],
       ),
-      body: Center(
+      body: Container(
           child: Column(
         children: [
-          Text('placeholder'),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            height: 550,
+            child: _mainpart,
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -127,5 +197,22 @@ class _MessagesPageState extends State<MessagesPage> {
         ],
       )),
     );
+  }
+
+  void LoadMembers() {
+    print("carregando membros");
+    //placeholder
+    Member c =
+        Member(id: 1, name: "bob", picture: "assets/images/placeholder.png");
+    Member d =
+        Member(id: 2, name: "todd", picture: "assets/images/placeholder.png");
+    Member e =
+        Member(id: 3, name: "chad", picture: "assets/images/placeholder.png");
+    final memberList = <Member>[c, d, e, c, d, e];
+    //placeholder
+    print("membros carregados");
+    setState(() {
+      _mainpart = ListView(children: MemberWidgetList(context, memberList));
+    });
   }
 }
